cd6e739ceb666e77be71d4a70dd027987ab27c73
droganov <serge@droganov.ru>
2021-08-17 21:12:11 +0300

    Validate piped rules

--- syncYobta/index.test.ts
+++ syncYobta/index.test.ts
@@ -1,20 +1,22 @@
-import { createRule } from '../createRule/index.js'
+import { numberYobta } from '../numberYobta/index.js'
+import { stringYobta } from '../stringYobta/index.js'
 import { syncYobta } from './index.js'
 
-let isNumber = createRule(<D>(data: D) => {
-  if (typeof data !== 'number') throw new Error('yobta!')
-  return data
-})
-
-let validate = syncYobta(isNumber)
+let validate = syncYobta(numberYobta('yobta!'))
 
 it('accepts valid', () => {
   let result = validate(1)
   expect(result).toEqual([1, null])
 })
 
+it('can pipe rules', () => {
+  let validateMultiple = syncYobta(numberYobta(), stringYobta())
+  let result = validateMultiple(1)
+  expect(result).toEqual(['1', null])
+})
+
 it('rejects invalid', () => {
-  let result = validate('1')
+  let result = validate([])
   expect(result).toEqual([
     null,
     [{ field: '@root', message: 'yobta!', path: [] }]
