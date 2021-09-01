ced5e54cd8cc98166654ce042495006f123ce158
droganov <serge@droganov.ru>
2021-09-01 12:47:37 +0300

    Update sync example and cover it with tests

--- src/syncYobta/index.test.ts
+++ src/syncYobta/index.test.ts
@@ -1,7 +1,13 @@
+import { booleanYobta } from '../booleanYobta'
+import { catchYobta } from '../catchYobta'
+import { fromEntriesYobta } from '../fromEntriesYobta'
 import { minCharactersYobta } from '../minCharactersYobta'
 import { numberYobta } from '../numberYobta'
+import { oneOfYobta } from '../oneOfYobta'
 import { requiredYobta } from '../requiredYobta'
+import { shapeYobta } from '../shapeYobta'
 import { stringYobta } from '../stringYobta'
+import { urlSearchParamsYobta } from '../urlSearchParamsYobta'
 import { syncYobta } from './'
 
 let validate = syncYobta(numberYobta('yobta!'))
@@ -24,4 +30,29 @@ it('can pipe rules', () => {
 it('rejects invalid', () => {
   let attempt = (): any => validate([])
   expect(attempt).toThrow('yobta!')
+})
+
+let validateSearch = syncYobta(
+  urlSearchParamsYobta(),
+  fromEntriesYobta(),
+  shapeYobta({
+    currentTab: [
+      catchYobta('tab-1', oneOfYobta<string[]>(['tab-1', 'tab-2', 'tab-3']))
+    ],
+    myModalIsOpen: [catchYobta(false, booleanYobta(), requiredYobta<boolean>())]
+  })
+)
+
+it("creates default state when can't extract it from url", () => {
+  expect(validateSearch('')).toEqual({
+    currentTab: 'tab-1',
+    myModalIsOpen: false
+  })
+})
+
+it('extracts state from url', () => {
+  expect(validateSearch('currentTab=tab-3&myModalIsOpen=true')).toEqual({
+    currentTab: 'tab-3',
+    myModalIsOpen: true
+  })
 })
