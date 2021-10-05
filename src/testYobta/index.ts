18074aed65bde7565f1e8579c931618d89125535
droganov <serge@droganov.ru>
2021-10-05 18:26:36 +0300

    testYobta to always expect a string

--- src/testYobta/index.ts
+++ src/testYobta/index.ts
@@ -3,10 +3,7 @@ import { ruleYobta, SyncRule } from '../ruleYobta'
 export const testMessage = 'Invalid format'
 
 interface TestFactory {
-  <I extends string | undefined>(
-    expression: RegExp,
-    message?: string,
-  ): SyncRule<I, I>
+  <I extends string>(expression: RegExp, message?: string): SyncRule<I, I>
 }
 
 export const testYobta: TestFactory = (
@@ -14,6 +11,6 @@ export const testYobta: TestFactory = (
   message = testMessage,
 ) =>
   ruleYobta(input => {
-    if (typeof input === 'undefined' || expression.test(input)) return input
+    if (expression.test(input)) return input
     throw new Error(message)
   })
