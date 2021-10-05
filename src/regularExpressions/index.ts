2afe16d64e3f045c1e46303b92e67df2b5b02f3c
droganov <serge@droganov.ru>
2021-10-05 17:18:26 +0300

    Add regular expression for testing slugs

---
+++ src/regularExpressions/index.ts
@@ -0,0 +1,8 @@
+export const reSlugYobta = /^[a-z][\da-z-]+[\da-z]+$/
+
+// RFC 5322
+/* eslint-disable security/detect-unsafe-regex */
+/* eslint-disable unicorn/better-regex */
+// TODO:
+export const reEmailYobta =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
