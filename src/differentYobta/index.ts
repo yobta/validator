556f53a0852366a627244ad1cda7f9c358a526ae
Pavel Brylev <pavel@cytlex.com>
2023-08-25 11:57:13 +0300

    differentYobta

--- src/differentYobta/index.ts
+++ src/differentYobta/index.ts
@@ -1,16 +1,17 @@
 import { ruleYobta, SyncRule } from '../ruleYobta/index.js'
 import { getIn } from '../_internal/getIn/index.js'
 import { Path } from '../_internal/createContext/index.js'
+import { getMessage } from '../_internal/getMessage/getMessage.js'
 
 export const differentMessage = (path: Path): string =>
   `It should be different from "${path.join('.')}"`
 
 export function differentYobta<I>(
   path: Path,
-  message = differentMessage,
+  message: typeof differentMessage | string = differentMessage,
 ): SyncRule<any, I> {
   return ruleYobta((input, { data }) => {
-    if (input === getIn(data, path)) throw new Error(message(path))
+    if (input === getIn(data, path)) throw new Error(getMessage(message, path))
     return input
   })
 }
